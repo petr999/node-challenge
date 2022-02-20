273e1a6e3c7a2db11b08a4bf42ffd0901450ab4f
Peter Vereshagin <peter@vereshagin.org>
2022-02-20 16:44:53 +0000

    style: :art: lint

--- test/acceptance/domains/user.test.ts
+++ test/acceptance/domains/user.test.ts
@@ -1,9 +1,8 @@
 import { Api } from '../utils/api';
 
 describe('Unknown user not found', () => {
-    test('Random user returns Not Found', (done) => {
-      Api.get('/user/v1/get-user-details?userId=12992732-bd00-4a9a-9426-131bd618740e')
-        .expect(404, done);
-    });
+  test('Random user returns Not Found', (done) => {
+    Api.get('/user/v1/get-user-details?userId=12992732-bd00-4a9a-9426-131bd618740e')
+      .expect(404, done);
   });
-
+});
