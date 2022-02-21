51788dfdf2de8c74f45b7f16ec1083f4ab045d2d
Peter Vereshagin <peter@vereshagin.org>
2022-02-21 12:35:04 +0000

    test: :art: fix scope/names of inline fixtures

--- packages/domains/expense/tests/db-get-user-expenses.test.ts
+++ packages/domains/expense/tests/db-get-user-expenses.test.ts
@@ -2,6 +2,9 @@ import { Connection } from '@nc/utils/dal';
 import { getUserExpenses } from '../handlers/get-user-expenses';
 import { mockConnection } from './utils/mocks/dal';
 
+const emptyUserExpenses = [[], 0] as [[], number];
+const fakeUserId = '98925892-8d0c-4cd3-bdbd-e2249e15900f';
+
 describe('Get User Expenses', () => {
   let connection: Connection;
   let getRepositoryMock: jest.SpyInstance<any, unknown[]>;
@@ -16,17 +19,14 @@ describe('Get User Expenses', () => {
     getRepositoryMock.mockRestore();
   });
 
-  const fakeUserExpenses = [[], 0] as [[], number];
-  const fakeUserId = '98925892-8d0c-4cd3-bdbd-e2249e15900f';
-
   test('getUserExpenses', async () => {
     // Arrange
     getRepositoryMock.mockImplementationOnce(() => ({
-      findAndCount: () => new Promise((resolve) => resolve(fakeUserExpenses)),
+      findAndCount: () => new Promise((resolve) => resolve(emptyUserExpenses)),
     }));
 
     // Assess
-    expect(await getUserExpenses(fakeUserId, {})).toEqual(fakeUserExpenses);
+    expect(await getUserExpenses(fakeUserId, {})).toEqual(emptyUserExpenses);
     expect(getRepositoryMock).toBeCalledTimes(1);
   });
 });
