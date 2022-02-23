cb986cfedf5adae5c79a8cdc58f8523e8f348d4f
Peter Vereshagin <peter@vereshagin.org>
2022-02-23 09:55:30 +0000

    style: :art: lint user acceptance

--- test/acceptance/domains/user.test.ts
+++ test/acceptance/domains/user.test.ts
@@ -11,11 +11,10 @@ describe('User found in database', () => {
   test('User known to be contained in database got fetched', (done) => {
     Api.get('/user/v1/get-user-details?userId=da140a29-ae80-4f0e-a62d-6c2d2bc8a474')
       .expect(200)
-      .end((err, res) =>{
-        expect(err).toBeFalsy()
-        expect(res.body).toMatchSnapshot(`user-details-by-id-da140a29-ae80-4f0e-a62d-6c2d2bc8a474`)
-        done()
-      })
-      ;
+      .end((err, res) => {
+        expect(err).toBeFalsy();
+        expect(res.body).toMatchSnapshot('user-details-by-id-da140a29-ae80-4f0e-a62d-6c2d2bc8a474');
+        done();
+      });
   });
 });
