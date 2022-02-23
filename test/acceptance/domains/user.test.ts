63b45b4126b76c1da040782a70876d7d162ec6fb
Peter Vereshagin <peter@vereshagin.org>
2022-02-23 09:38:26 +0000

    test: :white_check_mark: acceptance for 200

--- test/acceptance/domains/user.test.ts
+++ test/acceptance/domains/user.test.ts
@@ -6,3 +6,16 @@ describe('Unknown user not found', () => {
       .expect(404, done);
   });
 });
+
+describe('User found in database', () => {
+  test('User known to be contained in database got fetched', (done) => {
+    Api.get('/user/v1/get-user-details?userId=da140a29-ae80-4f0e-a62d-6c2d2bc8a474')
+      .expect(200)
+      .end((err, res) =>{
+        expect(err).toBeFalsy()
+        expect(res.body).toMatchSnapshot(`user-details-by-id-da140a29-ae80-4f0e-a62d-6c2d2bc8a474`)
+        done()
+      })
+      ;
+  });
+});
